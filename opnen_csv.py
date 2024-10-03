73dac94867012618aa894e2c33726702907145b9
Popescu Robert-Daniel <popescurobertdaniel99@gmail.com>
2024-10-03 18:08:00 +0300

    Refactor code to improve readability and maintainability

--- opnen_csv.py
+++ opnen_csv.py
@@ -13,11 +13,18 @@ csv_file_path = '3firme_neradiate_cu_sediu_2024-07-07.csv'
 already_processed_file_path = 'already_processed.xlsx'
 errors_file_path = 'errors.xlsx'
 no_email_file_path = 'no_email.xlsx'
+company_data_array_file_path = '3firme_neradiate_cu_sediu_2024-07-07.xlsx'
 
-# Initiate an array for storing the company dataq
+# Initiate an array for storing the company dataqqq
 company_data_array = []
 
-# Open the already_processed file and read the CUIs
+try:
+    df = pd.read_excel(company_data_array_file_path)
+    company_data_array = df.to_dict('records')
+except FileNotFoundError:
+    print("No company_data_array file found")
+
+# Open the already_processed file and read the CUIsq
 already_processed = []
 
 # Check if the file exists
@@ -30,7 +37,7 @@ except FileNotFoundError:
 
 no_email = []
 
-# Open the no_email file and read the CUIs
+# Open the no_email file and read the CUIsqqq
 try:
     df = pd.read_excel(no_email_file_path)
     no_email = df[0].tolist()
@@ -41,7 +48,7 @@ except FileNotFoundError:
 
 errors = []
 
-# Open the errors file and read the CUIs
+# Open the errors file and read the CUIsq
 try:
     df = pd.read_excel(errors_file_path)
     errors = df[0].tolist()
@@ -74,12 +81,12 @@ def extract_data():
                 print(f"Company {i+1} with CUI {row['CUI']} has already been processed")
                 continue
 
-            # Check if the company has no email
+            # Check if the company has no emailq
             if row['CUI'] in no_email:
                 print(f"Company {i+1} with CUI {row['CUI']} has no email")
                 continue
 
-            # Filter the rows by the desired county
+            # Filter the rows by the desired countyqq
             if ADR_JUDET_FILTER in row['ADR_JUDET']:
                 # Get the company CUI
                 company_cui = row['CUI']
@@ -87,26 +94,26 @@ def extract_data():
                 try:
                     # Get the company data
                     company_data = get_company_data(company_cui)
-                    # Sleep for 1 secod to avoid getting blockedq
-                    time.sleep(1)
+                    # Sleep for 1 secod to avoid getting blockedqq
+                    time.sleep(0.1)
                 except Exception as e:
                     try:
                         print(f"Failed to get data for company with CUI {company_cui}: {e}")
                         print("Retrying in 1 second...")
-                        time.sleep(1)
+                        time.sleep(0.1)
                         # Retry getting the company data
                         company_data = get_company_data(company_cui)
                         # Sleep for 1 secod to avoid getting blocked
-                        time.sleep(1)
+                        time.sleep(0.1)
                     except Exception as e:
                         print(f"Failed to get data for company with CUI {company_cui}: {e}")
                         errors.append(str(company_cui))
-                        # Write errors to an xlsx file
+                        # Write errors to an xlsx fileq
                         df = pd.DataFrame(errors)
                         df.to_excel('errors.xlsx', index=False)
                         continue
                 
-                # Extract the first autocomplete element
+                # Extract the first autocomplete elementq
                 first_autocomplete = company_data['data']['autocomplete'][0]
 
                 # Extract the primaryEmail
@@ -124,7 +131,7 @@ def extract_data():
                 # Extract the last CAEN
                 last_caen = first_autocomplete['caen'][-1]
 
-                # Create a row with the complete and updated data
+                # Create a row with the complete and updated dataq
                 row = {
                     **row,
                     'primaryEmail': primary_email,
@@ -136,9 +143,10 @@ def extract_data():
                 company_data_array.append(row)
                 # Write all the data from company_data_array to a new xlsx file
                 df = pd.DataFrame(company_data_array)
-                df.to_excel('3firme_neradiate_cu_sediu_2024-07-07.xlsx', index=False)
+                df.to_excel(company_data_array_file_path, index=False)
+                    
+                    
                 already_processed.append(str(company_cui))
-
                 # Write already_processed to an xlsx file
                 df = pd.DataFrame(already_processed)
                 df.to_excel('already_processed.xlsx', index=False)
@@ -148,7 +156,7 @@ def extract_data():
 
 
 if __name__ == '__main__':
-    # Start the keypress listener threadq
+    # Start the keypress listener thread
     keypress_thread = threading.Thread(target=listen_for_keypress)
     keypress_thread.start()
 
